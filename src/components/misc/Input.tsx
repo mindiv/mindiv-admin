c79ac5707f587e7577ac2f939ff023a3f81f8db3
jerry <jerrynwosu007@gmail.com>
2023-02-04 18:19:43 +0100

    update auth fully to tailwind

--- src/components/misc/Input.tsx
+++ src/components/misc/Input.tsx
@@ -22,13 +22,21 @@ export const CInput = ({
     formState: { errors },
   } = method;
   return (
-    <InputWrap>
-      <label>{label}</label>
-      <input type={type} {...rest} {...register(`${name}`)} />
+    <div className="mb-6 flex flex-col">
+      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        {label}
+      </label>
+      <input
+        type={type}
+        {...rest}
+        {...register(`${name}`)}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="name@flowbite.com"
+      />
       {errors[name]?.message && (
         <ErrorText text={errors[name]?.message || ''} />
       )}
-    </InputWrap>
+    </div>
   );
 };
 
