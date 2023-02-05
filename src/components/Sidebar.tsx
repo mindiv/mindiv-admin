f2d1a15d513ec353cf4b9abc11e784dca10c2190
jerry <jerrynwosu007@gmail.com>
2023-02-05 13:04:31 +0100

    dashboard stuff

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,4 @@
 import { Link, NavLink } from 'react-router-dom';
-import styled from 'styled-components';
 import {
   IoLibrary,
   IoStatsChart,
@@ -10,6 +9,8 @@ import {
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { clearAuthTokenFromStorage } from '../features/authSlice';
 import { setThemeMode } from '../features/appSlice';
+import { useState } from 'react';
+import ToggleTheme from './misc/ToggleTheme';
 
 const links = [
   { name: 'Dasboard', url: '/', icon: <IoStatsChart /> },
@@ -19,153 +20,36 @@ const links = [
 
 const Sidebar = () => {
   return (
-    <SidebarWrap>
+    <div className="flex flex-col px-5 py-5 w-64 transition-transform justify-between bg-gray-100 dark:bg-gray-900 dark:text-gray-200">
       <div>
-        <Logo>
+        <div className="pb-8 text-2xl">
           <Link to="/">Mindiv Admin</Link>
-        </Logo>
-        <NavLinks>
+        </div>
+        <div className="">
           {links.map((link, index) => (
             <NavLink
-              className={({ isActive }) => (isActive ? 'active' : undefined)}
+              className={({ isActive }) =>
+                (isActive
+                  ? 'active dark:bg-gray-700 dark:hover:text-gray-200 bg-gray-300'
+                  : '') +
+                ' flex items-center p-3 text-base font-normal rounded-lg hover:text-grey-100 dark:hover:text-gray-400'
+              }
               to={link.url}
               key={index}
             >
-              <span className="link-icon">{link.icon}</span>
+              <span className="mr-5 transition duration-75 text-gray-500 dark:text-gray-400">
+                {link.icon}
+              </span>
               <span className="link-text">{link.name}</span>
             </NavLink>
           ))}
-        </NavLinks>
+        </div>
       </div>
       <div>
-        <User />
+        <ToggleTheme />
       </div>
-    </SidebarWrap>
+    </div>
   );
 };
-
-const User = () => {
-  const dispatch = useAppDispatch();
-  const { mode } = useAppSelector((state) => state.app);
-
-  const onTheme = () => {
-    dispatch(setThemeMode(mode === 'light' ? 'dark' : 'light'));
-  };
-  const onLogout = () => {
-    dispatch(clearAuthTokenFromStorage());
-  };
-  return (
-    <UserWrap>
-      <div className="user-avatar"></div>
-      <p className="user-name">Jerry Nwosu</p>
-      <p className="user-email">jerrynwosu007@gmail.com</p>
-      <Actions>
-        <button title="Toggle Theme" onClick={onTheme}>
-          <IoMoon />
-        </button>
-        <button title="Logout" onClick={onLogout}>
-          <IoLogOut />
-        </button>
-      </Actions>
-    </UserWrap>
-  );
-};
-
-const SidebarWrap = styled.div`
-  min-width: 250px;
-  max-width: 250px;
-  background-color: ${(props) => props.theme.colors.background};
-  padding: 30px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  border-right: 1px solid ${(props) => props.theme.colors.borderColor};
-`;
-
-const NavLinks = styled.div`
-  display: flex;
-  flex-direction: column;
-  a {
-    padding: 12px 15px;
-    border-radius: 6px;
-    text-decoration: none;
-    color: ${(props) => props.theme.colors.text};
-    display: flex;
-    align-items: center;
-    align-items: center;
-    font-size: 14px;
-    font-weight: 600;
-
-    span {
-      display: flex;
-      align-items: center;
-    }
-
-    span.link-icon {
-      margin-right: 10px;
-      font-size: 20px;
-    }
-  }
-  .active {
-    background-color: ${(props) => props.theme.colors.accent};
-    color: #fff;
-  }
-`;
-
-const Logo = styled.div`
-  margin-bottom: 50px;
-  a {
-    margin-bottom: 50px;
-    text-decoration: none;
-    color: ${(props) => props.theme.colors.text};
-    font-size: 30px;
-    font-weight: 900;
-  }
-`;
-
-const UserWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  .user-avatar {
-    width: 60px;
-    height: 60px;
-    background-color: #888;
-    border-radius: 50%;
-    margin-bottom: 10px;
-  }
-
-  .user-name {
-    font-weight: 600;
-    color: ${(props) => props.theme.colors.text};
-  }
-
-  .user-email {
-    color: ${(props) => props.theme.colors.text};
-    font-size: 14px;
-  }
-`;
-
-const Actions = styled.div`
-  display: flex;
-  margin-top: 20px;
-  gap: 20px;
-
-  button {
-    width: 38px;
-    height: 38px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 18px;
-    background-color: ${(props) => props.theme.colors.secondary};
-    border: 1px solid ${(props) => props.theme.colors.borderColor};
-    outline: none;
-    border-radius: 5px;
-    cursor: pointer;
-    color: ${(props) => props.theme.colors.text};
-  }
-`;
 
 export default Sidebar;
--- src/pages/Dasboard.tsx
+++ src/pages/Dasboard.tsx
@@ -2,34 +2,17 @@ import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
-import { Container } from '../styles/GlobalStyle';
 
 const Dasboard = () => {
   return (
-    <Wrap>
+    <div className="flex h-screen bg-gray-100 dark:bg-gray-900">
       <Sidebar />
-      <Main>
+      <div className="flex flex-col flex-1">
         <Header />
-        <Container>
-          <Outlet />
-        </Container>
-      </Main>
-    </Wrap>
+        <Outlet />
+      </div>
+    </div>
   );
 };
-
-const Wrap = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100%;
-  background-color: ${(props) => props.theme.colors.background};
-`;
-
-const Main = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  width: 100%;
-`;
 
 export default Dasboard;
