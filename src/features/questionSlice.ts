885e13ffdd1895b53fdd11a99965a23d13036c52
jerry <jerrynwosu007@gmail.com>
2023-02-09 01:18:54 +0100

    add page in to state

--- src/features/questionSlice.ts
+++ src/features/questionSlice.ts
@@ -7,12 +7,14 @@ type CategoryState = {
   status: 'idle' | 'loading' | 'succeeded' | 'failed';
   questions: [];
   error: any;
+  info: any;
 };
 
 const initialState: CategoryState = {
   status: 'idle',
   questions: [],
   error: null,
+  info: null,
 };
 
 export const getQuestions = createAsyncThunk(
@@ -50,7 +52,8 @@ const questionSlice = createSlice({
       })
       .addCase(getQuestions.fulfilled, (state, action: PayloadAction<any>) => {
         state.status = 'succeeded';
-        state.questions = action.payload.payload;
+        state.questions = action.payload.payload.questions;
+        state.info = action.payload.payload.pageInfo;
       })
       .addCase(
         getQuestions.rejected,
